515cc060adbe59c7064bdbc92f750dedc983b720
irlansuzano <irlanmsuzano@hotmail.com>
2022-09-21 23:12:42 -0300

    Criação do metodo findById no SellerDaoJDBC

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,23 @@
 package model.dao.impl;
 
+import db.DB;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller seller) {
@@ -22,8 +34,46 @@ public class SellerDaoJDBC implements SellerDao {
 
     }
 
+    /**
+     * Monta o objeto seller
+     * @param id
+     * @return Seller
+     */
     @Override
     public Seller findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT seller.*, department.Name as DepName " +
+                    "From seller INNER JOIN department" +
+                    "ON seller.DepartmentId = department.Id" +
+                    "WHERE seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if(rs.next()){
+                Department dp = new Department();
+                Seller seller = new Seller();
+                dp.setId(rs.getInt("DepartmentId"));
+                dp.setName(rs.getString("DepName"));
+                seller.setId(rs.getInt("Id"));
+                seller.setName(rs.getString("Name"));
+                seller.setEmail(rs.getString("Email"));
+                seller.setBaseSalary(rs.getDouble("BaseSalary"));
+                seller.setBirthDate(rs.getDate("BirthDate"));
+                seller.setDepartment(dp);
+                return seller;
+            }
+            return null;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
+
         return null;
     }
 
